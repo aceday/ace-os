d3ab2222491cc87ee76eff30b6b3c671b620e344
Marc Buday <68708541+aceday@users.noreply.github.com>
2025-07-05 01:09:51 +0800

    Comment gc and vs

--- build_files/03-package.sh
+++ build_files/03-package.sh
@@ -53,17 +53,17 @@ fi
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O /tmp/chrome.rpm
 # dnf5 install -y /tmp/chrome.rpm
 # rm -f /tmp/chrome.rpm
-wget https://dl.google.com/linux/linux_signing_key.pub
-sudo rpm --import linux_signing_key.pub
-dnf5 update -y
-dnf5 install -y google-chrome-stable
+#wget https://dl.google.com/linux/linux_signing_key.pub
+#sudo rpm --import linux_signing_key.pub
+#dnf5 update -y
+#dnf5 install -y google-chrome-stable
 
 
 # VS Code
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update -y
-dnf install -y code
+#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+#echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+#dnf check-update -y
+#dnf install -y code
 
 
 
