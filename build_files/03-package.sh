62ad819845486ffe71ffa13e469292d12296222d
aceday <markcediebuday@gmail.com>
2025-07-05 00:30:03 +0800

    Add vs code

--- build_files/03-package.sh
+++ build_files/03-package.sh
@@ -49,10 +49,22 @@ if [ ${#PKGS_TO_INSTALL[@]} -gt 0 ]; then
 fi
 
 # Google Chrome
-rm -rf /opt/google/
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O /tmp/chrome.rpm
-dnf5 install -y /tmp/chrome.rpm
-rm -f /tmp/chrome.rpm
+# rm -rf /opt/google/
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O /tmp/chrome.rpm
+# dnf5 install -y /tmp/chrome.rpm
+# rm -f /tmp/chrome.rpm
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo rpm --import linux_signing_key.pub
+dnf5 update
+dnf5 install -y google-chrome
+
+
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+dnf install -y code
+
 
 
 echo "::endgroup::"
