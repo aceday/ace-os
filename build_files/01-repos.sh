5c45ca61eee84e7c33bccac677545fca6e7c37d4
aceday <markcediebuday@gmail.com>
2025-07-04 22:30:50 +0800

    Disable some repos for bluefin

--- build_files/01-repos.sh
+++ build_files/01-repos.sh
@@ -46,13 +46,13 @@ done
 unset COPR
 
 # Add Terra repo (custom repopath)
-dnf5 -y install --nogpgcheck \
-    --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' \
-    terra-release terra-release-extras
+# dnf5 -y install --nogpgcheck \
+#     --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' \
+#     terra-release terra-release-extras
 
 # Add Tailscale repo
-dnf5 -y config-manager addrepo --overwrite \
-    --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+# dnf5 -y config-manager addrepo --overwrite \
+#     --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
 # Install RPMFusion repos
 dnf5 -y install \
@@ -60,25 +60,25 @@ dnf5 -y install \
     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 # Enable Negativo17 multimedia repo
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+# sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-# add additional negativo17 repos
-dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
-dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-rar.repo
+# # add additional negativo17 repos
+# dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
+# dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-rar.repo
 
-# set priorities and exclusions
-dnf5 -y config-manager setopt "*bazzite*".priority=1
-dnf5 -y config-manager setopt "*akmods*".priority=2
-dnf5 -y config-manager setopt "terra-mesa".enabled=true
-dnf5 -y config-manager setopt "terra-nvidia".enabled=false
+# # set priorities and exclusions
+# dnf5 -y config-manager setopt "*bazzite*".priority=1
+# dnf5 -y config-manager setopt "*akmods*".priority=2
+# dnf5 -y config-manager setopt "terra-mesa".enabled=true
+# dnf5 -y config-manager setopt "terra-nvidia".enabled=false
 
-# negativo17
-eval "$(/ctx/dnf5-setopt.sh setopt '*negativo17*' priority=4 exclude='mesa-* *xone*')"
+# # negativo17
+# eval "$(/ctx/dnf5-setopt.sh setopt '*negativo17*' priority=4 exclude='mesa-* *xone*')"
 
-dnf5 -y config-manager setopt "*".exclude="*.aarch64"
-dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
-dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
-dnf5 -y config-manager setopt "*staging*".exclude="scx-scheds kf6-* mesa* mutter* rpm-ostree* systemd* gnome-shell gnome-settings-daemon gnome-control-center gnome-software libadwaita tuned*"
+# dnf5 -y config-manager setopt "*".exclude="*.aarch64"
+# dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
+# dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
+# dnf5 -y config-manager setopt "*staging*".exclude="scx-scheds kf6-* mesa* mutter* rpm-ostree* systemd* gnome-shell gnome-settings-daemon gnome-control-center gnome-software libadwaita tuned*"
 
 # TLP UI
 sudo dnf copr enable sunwire/tlpui
