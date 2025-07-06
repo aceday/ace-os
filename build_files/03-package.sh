8c29ab903de59e97e1a55d67997805bb8919555a
aceday <markcediebuday@gmail.com>
2025-07-07 00:08:10 +0800

    Test chrome inst

--- build_files/03-package.sh
+++ build_files/03-package.sh
@@ -82,12 +82,36 @@ fi
 # Google Chrome
 # rm -rf /opt/google/
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O /tmp/chrome.rpm
-# dnf5 install -y /tmp/chrome.rpm
+# rpm-ostree install -y /tmp/chrome.rpm
 # rm -f /tmp/chrome.rpm
 #wget https://dl.google.com/linux/linux_signing_key.pub
 #sudo rpm --import linux_signing_key.pub
 #dnf5 update -y
 #dnf5 install -y google-chrome-stable
+set -oue pipefail
+mkdir -p /usr/share/ublue-tr/chrome-workarounds
+mkdir -p /tmp/chrome-workarounds
+echo "Downloading Google Signing Key"
+curl https://dl.google.com/linux/linux_signing_key.pub > /usr/share/ublue-tr/chrome-workarounds/linux_signing_key.pub
+
+rpm --import /usr/share/ublue-tr/chrome-workarounds/linux_signing_key.pub
+
+echo "collecting information on where rpm put the key for future reference"
+ls -l /etc/pki/rpm-gpg | grep -v fedora | grep -v rpmfusion 
+rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com'
+
+echo "Downloading Google Chrome"
+curl https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm > /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm
+echo "Verifying Google Chrome"
+rpm -K /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm
+# Save so we can verify the version later
+TODAYS_CHROME_VERSION=$(rpm -qp --queryformat '%{VERSION}' /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm)
+echo $TODAYS_CHROME_VERSION > /usr/share/ublue-tr/chrome-workarounds/google-chrome-current-version
+
+echo "Verified Google Chrome RPM containing $TODAYS_CHROME_VERSION"
+
+
+
 
 
 # VS Code
