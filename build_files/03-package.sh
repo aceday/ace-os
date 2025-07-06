056a69aaaba257c2dec8169d36a997aff3af7536
aceday <markcediebuday@gmail.com>
2025-07-07 02:31:37 +0800

    change fish

--- build_files/03-package.sh
+++ build_files/03-package.sh
@@ -49,9 +49,11 @@ PKGS_TO_INSTALL=(
     duf
     ncdu
     android-tools
-    zsh
-    zsh-autocomplete
-    zsh-autosuggestions
+    fish
+    #zsh
+    #zsh-autocomplete
+    #zsh-autosuggestions
+    hashcat
     util-linux
     # scrcpy
 
@@ -111,7 +113,7 @@ TODAYS_CHROME_VERSION=$(rpm -qp --queryformat '%{VERSION}' /usr/share/ublue-tr/c
 echo $TODAYS_CHROME_VERSION > /usr/share/ublue-tr/chrome-workarounds/google-chrome-current-version
 
 echo "Verified Google Chrome RPM containing $TODAYS_CHROME_VERSION"
-dnf5 install -y google-chrome-stable
+# dnf5 install -y google-chrome-stable
 
 
 
