00382a84772464f9366da597a8b563e1a2c999ef
aceday <markcediebuday@gmail.com>
2025-07-16 03:37:40 +0800

    Test install vscode rpm

--- build_files/03-package.sh
+++ build_files/03-package.sh
@@ -175,7 +175,7 @@ echo "Verified Google Chrome RPM containing $TODAYS_CHROME_VERSION"
 # dnf5 install -y google-chrome-stable
 
 # VS Code v2
-wget https://update.code.visualstudio.com/latest/linux-rpm-x64/stable -O code-latest-x64.rpm
+wget --no-check-certificate https://update.code.visualstudio.com/latest/linux-rpm-x64/stable -O code-latest-x64.rpm
 sudo dnf install ./code-latest-x64.rpm
 rm ./code-latest-x64.rpm
 
