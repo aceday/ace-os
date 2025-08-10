9a2e31d869b2b973a667610dc1f06b344a224268
aceday <markcediebuday@gmail.com>
2025-08-11 07:52:22 +0800

    Add intel_iommu=on

--- build_files/09-config.sh
+++ build_files/09-config.sh
@@ -8,6 +8,13 @@ else
     echo 'GRUB_DISABLE_OS_PROBER=false' >> "$grub_cfg"
 fi
 
+# Configure GRUB command line options
+if grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=' "$grub_cfg"; then
+    sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_iommu=on"/' "$grub_cfg"
+else
+    echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_iommu=on"' >> "$grub_cfg"
+fi
+
 
 # TLP Service
 cp ./config/tlp.conf /etc/tlp.conf
