5b1fac7e026c01edf46dff2af8de35437f6bcfe1
aceday <markcediebuday@gmail.com>
2025-08-11 08:16:08 +0800

    Test intel_kvm

--- build_files/09-config.sh
+++ build_files/09-config.sh
@@ -10,9 +10,9 @@ fi
 
 # Configure GRUB command line options
 if grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=' "$grub_cfg"; then
-    sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_iommu=on"/' "$grub_cfg"
+    sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_iommu=on iommu=pt kvm.ignore_msrs=1"/' "$grub_cfg"
 else
-    echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_iommu=on"' >> "$grub_cfg"
+    echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_iommu=on iommu=pt kvm.ignore_msrs=1"' >> "$grub_cfg"
 fi
 
 
@@ -26,10 +26,21 @@ tlp start
 # chsh -s /bin/zsh
 
 
-# VM
+# VM - KVM/QEMU setup for Fedora Silverblue
 sed -i 's/^#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/' /etc/libvirt/libvirtd.conf
 sed -i 's/^#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
 sed -i 's/^#user = "qemu"/user = "mac"/' /etc/libvirt/qemu.conf
+
+# Enable and start libvirtd service
+systemctl enable --now libvirtd.service
+systemctl enable --now virtqemud.service
+
+# Add user to libvirt group (will take effect after reboot)
+usermod -a -G libvirt mac
+
+# Load KVM modules
+modprobe kvm
+modprobe kvm_intel  # Use kvm_amd for AMD processors
 
 # Test kernel print
 echo "Kernel version: $(uname -r)"
