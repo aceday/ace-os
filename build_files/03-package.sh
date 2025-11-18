58dbb7ba97ee7570dd6e91847797905cd22228b9
Marc Buday <68708541+aceday@users.noreply.github.com>
2025-11-18 13:18:00 +0800

    Clean up package installation script
    Removed commented-out installation instructions for various packages and Google Chrome.

--- build_files/03-package.sh
+++ build_files/03-package.sh
@@ -126,15 +126,7 @@ PKGS_TO_INSTALL=(
 
     gutenprint
     gutenprint-cups
-    lprint
 )
-
-    # python3.13-devel
-    # btrfs-assistant # BTRFS GUI tool
-    #tlp
-    #tlp-rdw
-    # tlpui
-
 if [ ${#PKGS_TO_INSTALL[@]} -gt 0 ]; then
     dnf5 install -y "${PKGS_TO_INSTALL[@]}"
 fi
@@ -150,58 +142,10 @@ if [ ${#PKGS_TO_UNINSTALL[@]} -gt 0 ]; then
     dnf5 remove -y "${PKGS_TO_UNINSTALL[@]}"
 fi
 
-# Google Chrome
-# rm -rf /opt/google/
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O /tmp/chrome.rpm
-# rpm-ostree install -y /tmp/chrome.rpm
-# rm -f /tmp/chrome.rpm
-#wget https://dl.google.com/linux/linux_signing_key.pub
-#sudo rpm --import linux_signing_key.pub
-#dnf5 update -y
-#dnf5 install -y google-chrome-stable
-
-# # Google Chrome V2
-# rm -rf /opt/google/
-
-# mkdir -p /usr/share/ublue-tr/chrome-workarounds
-# mkdir -p /tmp/chrome-workarounds
-# echo "Downloading Google Signing Key"
-# curl https://dl.google.com/linux/linux_signing_key.pub > /usr/share/ublue-tr/chrome-workarounds/linux_signing_key.pub
-
-# rpm --import /usr/share/ublue-tr/chrome-workarounds/linux_signing_key.pub
-
-# echo "collecting information on where rpm put the key for future reference"
-# ls -l /etc/pki/rpm-gpg | grep -v fedora | grep -v rpmfusion 
-# rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com'
-
-# echo "Downloading Google Chrome"
-# curl https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm > /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm
-# echo "Verifying Google Chrome"
-# rpm -K /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm
-# # Save so we can verify the version later
-# TODAYS_CHROME_VERSION=$(rpm -qp --queryformat '%{VERSION}' /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm)
-# echo $TODAYS_CHROME_VERSION > /usr/share/ublue-tr/chrome-workarounds/google-chrome-current-version
-
-# echo "Verified Google Chrome RPM containing $TODAYS_CHROME_VERSION"
-# # dnf5 install -y /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm
-# # rm /usr/share/ublue-tr/chrome-workarounds/google-chrome-stable_current_x86_64.rpm
-
 # VS Code Native
 wget --no-check-certificate https://update.code.visualstudio.com/latest/linux-rpm-x64/stable -O code-latest-x64.rpm
 sudo dnf5 install -y ./code-latest-x64.rpm
 rm ./code-latest-x64.rpm
-
-# Virtualbox based from rpm
-# wget --no-check-certificate https://download.virtualbox.org/virtualbox/7.1.12/VirtualBox-7.1-7.1.12_169651_fedora40-1.x86_64.rpm
-# sudo dnf5 install -y ./VirtualBox-7.1-7.1.12_169651_fedora40-1.x86_64.rpm
-# rm ./VirtualBox-7.1-7.1.12_169651_fedora40-1.x86_64.rpm
-# /sbin/vboxconfig
-
-#git clone https://github.com/ZerBea/hcxdumptool.git
-#cd hcxdumptool
-#make -j $(nproc)
-#make install
-#make install PREFIX=/usr/local (as super user)
 
 echo "::endgroup::"
 
