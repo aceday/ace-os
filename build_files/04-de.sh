d7566a360450c4c52ffdedf62de0b2bb043babcf
aceday <markcediebuday@gmail.com>
2025-12-27 19:39:39 +0800

    Remove 50-* in dms

--- build_files/04-de.sh
+++ build_files/04-de.sh
@@ -58,30 +58,30 @@ paru -S --noconfirm --needed $AUR_PKGS_STR
 "
 pacman -S --noconfirm "${packages[@]}"
 
-cat << 'EOF' > /etc/nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json
-{
-    "rules": [
-        {
-            "pattern": {
-                "feature": "procname",
-                "matches": "niri"
-            },
-            "profile": "Limit Free Buffer Pool On Wayland Compositors"
-        }
-    ],
-    "profiles": [
-        {
-            "name": "Limit Free Buffer Pool On Wayland Compositors",
-            "settings": [
-                {
-                    "key": "GLVidHeapReuseRatio",
-                    "value": 0
-                }
-            ]
-        }
-    ]
-}
-EOF
+# cat << 'EOF' > /etc/nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json
+# {
+#     "rules": [
+#         {
+#             "pattern": {
+#                 "feature": "procname",
+#                 "matches": "niri"
+#             },
+#             "profile": "Limit Free Buffer Pool On Wayland Compositors"
+#         }
+#     ],
+#     "profiles": [
+#         {
+#             "name": "Limit Free Buffer Pool On Wayland Compositors",
+#             "settings": [
+#                 {
+#                     "key": "GLVidHeapReuseRatio",
+#                     "value": 0
+#                 }
+#             ]
+#         }
+#     ]
+# }
+# EOF
 
 cat << 'EOF' > /etc/greetd/niri.kdl
 input {
